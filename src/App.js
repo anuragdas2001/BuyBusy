c2faf815e57bad9820ea20138efdaa179d339671
anuragdas2001 <anuragdas12921@gmail.com>
2024-03-22 07:28:06 -0700

    Added Navbar & Search Box

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navbar } from './components/Navbar';
+import { SearchBox } from './components/SearchBox';
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <SearchBox/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,17 @@
+export const SearchBox = () => {
+  return (
+    <>
+      <form class="d-flex" role="search">
+        <input
+          class="form-control ms-3 me-3 "
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        ></input>
+        <button class="btn btn-outline-success" type="submit">
+          Search
+        </button>
+      </form>
+    </>
+  );
+};
