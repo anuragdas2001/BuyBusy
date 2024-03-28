2872ecee7671e80c5d07e5e6282b78e113813dd5
anuragdas2001 <anuragdas12921@gmail.com>
2024-03-28 22:04:59 +0530

    Added FallBack UI

---
+++ src/components/FallBackUI/FallBackUI.jsx
@@ -0,0 +1,13 @@
+import styles from "../../styles/FallBackUI.module.css";
+export const FallBackUI = () => {
+  return (
+    <>
+      <div className={styles.errorContainer}>
+        <img
+          src="https://t3.ftcdn.net/jpg/02/15/06/84/240_F_215068406_S4qqbkegOu1Al2dQF3plwI3oWJvDWxYo.jpg"
+          alt=""
+        />
+      </div>
+    </>
+  );
+};
